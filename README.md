9a2cae6d53f68897dd7e4851d00689a138d94e31
jayakrishnanni <jayakrishnanni@gmail.com>
2022-07-20 09:23:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-zbsgms
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascript-zbsgms)
---
+++ index.js
@@ -0,0 +1,6 @@
+function abc() {
+  return document.write(5 + 6 + 'sachin'+ 6);
+  document.getElementById("demo").innerHTML="hello";
+}
+
+abc();
